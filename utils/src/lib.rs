0779f805f682bc7a1e5058c8956e61091a805676
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-08-27 02:19:31 +0100

    utils: introduce an utils to trace the library
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ utils/src/lib.rs
@@ -0,0 +1,7 @@
+//! Utils crater that implement a sequence of generics function
+//! to work with the monkey codegen but it is designed also to provided
+//! a utils crater to avoid to make double work in each project.
+//!
+//!
+//! author: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
+pub mod tracer;
